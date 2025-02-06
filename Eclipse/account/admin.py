b8fcec41316e1cb8e8f02859e7c0f5bf22172630
José Guilherme Gurgel Barbosa <20241094010026@ifrn.local>
2025-02-06 17:04:46 -0300

    carrinho css , endereços

--- Eclipse/account/admin.py
+++ Eclipse/account/admin.py
@@ -2,12 +2,16 @@
 from django.contrib import admin
 
 #models import
-from .models import Carriho
+from .models import Carriho,Endereço
 
 #models admin
 class CarrinhoAdmin(admin.ModelAdmin):
     list_display = ("User",'Produtos','Quantia')
     search_fields = ("User_username",'Produtos_Nome')
 
+class EndereçoAdmin(admin.ModelAdmin):
+    list_display= ("User","Cidade")
+
 # Register your models here.
 admin.site.register(Carriho,CarrinhoAdmin)
+admin.site.register(Endereço,EndereçoAdmin)
--- Eclipse/account/models.py
+++ Eclipse/account/models.py
@@ -12,3 +12,16 @@ class Carriho(models.Model):
     
     def __str__(self):
         return f'{self.User},{self.Produtos}'
+    
+class Endereço(models.Model):
+    User = models.ForeignKey(User,on_delete=models.CASCADE)
+    Cep = models.IntegerField(null=False)
+    Cidade = models.CharField(max_length=60,null=False)
+    Rua = models.CharField(max_length=60,null=False)
+    Endereço = models.IntegerField(null=False)
+    Complemento = models.CharField(max_length=100,null=False)
+    Referencia = models.TextField()
+    Telefone =  models.IntegerField()
+
+    def __str__(self) -> str:
+        return f"{self.User} - {self.Cidade}"
